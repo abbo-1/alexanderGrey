0dff83f32d40bf9655395bcd37ed5a2d60cdd0e6
abbo-1 <abbott.kevinj@gmail.com>
2021-08-13 17:37:18 -0500

    added typewriter effect for title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import Typewriter from 'typewriter-effect';
 
 function App() {
   return (
     <div className="App">
-
-        <p>
-          test text
-        </p>
+        <Typewriter
+  onInit={(typewriter) => {
+    typewriter.typeString('<span style="color: #ffffff;">Alexander Gray</span>')
+    
+      .callFunction(() => {
+        console.log('String typed out!');
+      })
+      .pauseFor(2500)
+      .start();
+  }}
+/>
 
 
     </div>
